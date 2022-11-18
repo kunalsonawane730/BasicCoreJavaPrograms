7370a93eec939e6c3acd4aaabf26c903cca29cfc
Kunal <kunalsonawane730@gmail.com>
2022-11-18 20:43:28 +0530

    adding welcome message for basic core java programs

---
+++ src/test/java/com/day5/basiccorejava/AppTest.java
@@ -0,0 +1,20 @@
+package com.day5.basiccorejava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
